e28e22c8190169d915b8373085a9b85235c0c4bf
Evandro Santos <vandersant7@gmail.com>
2024-02-13 18:13:04 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { AppRoutes } from './pages/routes'
+
+function App() {
+  
+
+  return (
+    <div>
+      <AppRoutes />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/routes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Posts } from "./posts";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export { AppRoutes };
